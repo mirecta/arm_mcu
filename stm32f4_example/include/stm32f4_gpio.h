cafe09550c5c8be7f67c3a8722457f2f140e214f
Miroslav Talasek <miroslav.talasek@seznam.cz>
2014-10-25 22:43:30 +0000

    start to write some help defines for GPIO

---
+++ stm32f4_example/include/stm32f4_gpio.h
@@ -0,0 +1,48 @@
+#ifndef _STM32F_GPIO_
+#define _STM32F_GPIO_
+
+
+
+#define GP_PIN0    0
+#define GP_PIN1    1
+#define GP_PIN2    2
+#define GP_PIN3    3
+#define GP_PIN4    4
+#define GP_PIN5    5
+#define GP_PIN6    6
+#define GP_PIN7    7
+#define GP_PIN8    8
+#define GP_PIN9    9
+#define GP_PIN10   10
+#define GP_PIN11   11
+#define GP_PIN12   12
+#define GP_PIN13   13
+#define GP_PIN14   14
+#define GP_PIN15   15
+
+
+#define GP_MODE_INPUT      0x00
+#define GP_MODE_OUTPUT     0x01
+#define GP_MODE_ALTERNATE  0x02
+#define GP_MODE_ANALOG     0x03
+
+#define GP_SPEED_LOW       0x00
+#define GP_SPEED_MEDIUM    0x01
+#define GP_SPEED_FAST      0x02
+#define GP_SPEED_HIGH      0x03
+
+#define GP_TYPE_PUPD       0x00
+#define GP_TYPE_OD         0x01
+
+#define GP_PULL_NONE       0x00
+#define GP_PULL_UP         0x01
+#define GP_PULL_DOWN       0x02
+
+
+
+
+
+
+
+
+#endif//_STM32F_GPIO_
