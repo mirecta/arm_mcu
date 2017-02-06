d192938a04e7c6cf407092e62f08210199a98a59
Miroslav Talasek <miroslav.talasek@seznam.cz>
2017-02-07 00:10:29 +0100

    test

---
+++ adafruit-gfx/blink.cc
@@ -0,0 +1,48 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2010 Gareth McMullin <gareth@blacksphere.co.nz>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+extern void  initialise_monitor_handles(void);
+
+
+
+int main(void)
+{
+    int i;
+
+    rcc_clock_setup_in_hse_8mhz_out_72mhz();
+    rcc_periph_clock_enable(RCC_GPIOC);
+    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
+            GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+#if defined(ENABLE_SEMIHOSTING) && (ENABLE_SEMIHOSTING) 
+    initialise_monitor_handles();
+#endif
+    while (1)
+    {
+        gpio_toggle(GPIOC, GPIO13); /* LED on/off */
+    printf("hello world\n");
+        for (i = 0; i < 2400000; i++)    /* Wait a bit. */
+            __asm__("nop");
+    }
+}
+
