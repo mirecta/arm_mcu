ef0574fe2c8e848224a8d4d7f9378584184f17e3
Miroslav Talasek <miroslav.talasek@seznam.cz>
2018-10-13 21:47:42 +0200

    return to stlink , breathe , alternate pin function

--- blink-semihosting-opencm3/blink.c
+++ blink-semihosting-opencm3/blink.c
@@ -21,28 +21,61 @@
 #include <stdlib.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/timer.h>
 
 extern void  initialise_monitor_handles(void);
-
-
-
+#define STEP 10
+//timer3 channel 4
+void delay(){
+    int i;
+for (i = 0; i < 50000; i++)    /* Wait a bit. */
+            __asm__("nop");
+}
 int main(void)
 {
     int i;
 
     rcc_clock_setup_in_hse_8mhz_out_72mhz();
     rcc_periph_clock_enable(RCC_GPIOC);
-    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
-            GPIO_CNF_OUTPUT_PUSHPULL, GPIO9);
+    rcc_periph_clock_enable(RCC_TIM3);
+    
+    rcc_periph_clock_enable(RCC_AFIO);
+    gpio_primary_remap(0,AFIO_MAPR_TIM3_REMAP_FULL_REMAP );
+    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_50_MHZ,
+            GPIO_CNF_OUTPUT_ALTFN_PUSHPULL , GPIO9);
+
+    timer_set_mode(TIM3, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE,
+               TIM_CR1_DIR_UP);
+    
+    timer_set_prescaler(TIM3,36);
+    timer_set_period(TIM3, 1000);
+    timer_set_oc_mode(TIM3, TIM_OC4, TIM_OCM_PWM1);
+    timer_enable_oc_output(TIM3, TIM_OC4);
+   // timer_enable_preload(TIM3);
+    //timer_continuous_mode(TIM3);
+    timer_enable_counter(TIM3);
+    
+    
+
+
 #if defined(ENABLE_SEMIHOSTING) && (ENABLE_SEMIHOSTING) 
     initialise_monitor_handles();
 #endif
+    uint16_t a = 0;
     while (1)
     {
-        gpio_toggle(GPIOC, GPIO9); /* LED on/off */
-    printf("hello world\n");
-        for (i = 0; i < 2400000; i++)    /* Wait a bit. */
-            __asm__("nop");
+        for(i=0; i< 1000; i++){
+        timer_set_oc_value(TIM3, TIM_OC4, i);
+        delay();
+        }
+        for(i=999; i >=0 ; i--){
+        timer_set_oc_value(TIM3, TIM_OC4, i);
+        delay();
+        }       
+        //gpio_toggle(GPIOC, GPIO9); /* LED on/off */
+    //printf("hello world\n");
+        
+        
     }
 }
 
